1ce135c07d65902f47c3e252ffda90e142341faf
minijbs07 <minijbs07@gmail.com>
2026-05-05 13:27:12 +0200

    Updates

--- _next/static/dqTXqbCeGfUy9ON-vxfIo/_buildManifest.js
+++ _next/static/dqTXqbCeGfUy9ON-vxfIo/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/bot//_next/:path+",
+        "destination": "/bot/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
